7e9ef788a4a20e3cb7bcf8dc57061f22badb83f9
IvanShmidt <mrchurilov@mail.ru>
2020-03-28 19:25:50 +0300

    initial

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+struct Student
+{
+  string Name;
+  string Surname;
+  int Day, Month, Year;
+};
+
+
+int main() {
+  
+  int n;
+  cin >> n;
+  cout << n;
+
+  return 0;
+}
+
