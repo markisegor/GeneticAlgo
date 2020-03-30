cc2b8b10ff114b14e3706b49b2fd34a58f018ced
ValienV <egorkakireev@gmail.com>
2020-03-30 19:16:31 +0300

    Добавляем первую версию функции main

--- main.cpp
+++ main.cpp
@@ -1,96 +1,67 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <ctime>
-
-#define PopSize 10
-#define ChromoDim 10
-#define NumIterations 100
 
 using namespace std;
 
+int dim, D, PopSize;    // глобальные переменные: dim - количество элементов, D - плотность рюкзака, PopSize - размер популяции
+double seconds;         // seconds - время работы алгоритма
 
-void ReadCSV(){
+void ReadCSV()
+{
     //from csv
     // TODO MAX
 }
 
 
-void WriteCSV(){
+void WriteCSV()
+{
     //to csv
     // TODO MAX
 }
 
-
-vector<int> TaskGeneration(int dim, int D) {
+vector<int> TaskGeneration(int dimension, int Density)
+{
     // возвращает веса элементов vector<int> (dim + int TargetWeight)
     // TODO MAX
 }
 
-
-vector<vector<bool>> PoulationGeneration(int PSize, int dim) {
-    /*generate a random population of chromosomes*/
-
-    srand(time(0));
-    vector<vector<bool>> Pop(PSize);
-
-    for (auto Сhromo : Pop) {
-        for (int i = 0; i < dim; ++i) {
-            Сhromo[i] = rand() % 2;
-        }
-    }
-
-    return Pop;
+void GeneticAlgo(vector<int> Task)
+{
+    /*
+    генерация начальной популяцции
+    цикл по i
+    TODO VANES
+    */
 }
 
 
-int FitnessFunction(vector<bool> X) {
-    // TODO AKELLA
-}
-
-
-vector<vector<bool>> Selection(vector<vector<bool>> Generation) {
-    // TODO DIMA
-}
-
-
-vector<bool> Crossingover(vector<vector<bool>> Generation) {
-    // TODO DIMA
-}
-
-
-vector<bool> Mutation(vector<bool> Generation) {
-    // TODO AKELLA
-}
-
-
-void GeneticAlgo(vector<int> Task) {
-    
-    unsigned int start_time =  clock();
-
-    //генерация начальной популяцции
-    vector<vector<bool>> InitPop = PoulationGeneration(PopSize, ChromoDim);
-
-    for (int i = 0; i < NumIterations; ++i) {
-
-        if (/*validate on exit*/) {
-            //
-        }
-    }
-    
-    unsigned int end_time = clock();
-    unsigned int work_time = end_time - start_time;
-    
-}
-
-
-void DynamicAlgo(vector<int> Task) {
+void DynamicAlgo(vector<int> Task)
+{
     /*
     TODO AZA
     */
 }
 
-
-int main() {
-    // TODO EGOR
+int main()
+{
+    ReadCSV();                                              //считываем исходные данные из файла
+    for (int k = 1; k <= 100; k++)                          //цикл для генерации задач
+    {
+        vector<int> Task = TaskGeneration(dim, D);          //Task - текущая задача
+        clock_t start1 = clock();                           //начало отсчета времени работы алгоритма
+        GeneticAlgo(Task);                                  //выполнение задачи методом генетического алгоритма
+        clock_t end1 = clock();                             //конец отсчета времени работы алгоритма
+        seconds = (double)(end1 - start1) / CLOCKS_PER_SEC; //вычисление времени работы алгоритма
+        WriteCSV();                                         //запись результатов работы алгоритма в файл
+        clock_t start2 = clock();                           //начало отсчета времени работы алгоритма
+        DynamicAlgo(Task);                                  //выполнение задачи методом динамического программирования
+        clock_t end2 = clock();                             //конец отсчета времени работы алгоритма
+        seconds = (double)(end2 - start2) / CLOCKS_PER_SEC; //вычисление времени работы алгоритма
+        WriteCSV();                                         //запись результатов работы алгоритма в файл
+    };
+    return 0;
 }
