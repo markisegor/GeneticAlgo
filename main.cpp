36c66e10aab222c9e57e631ee1ce6945d5262c60
DimaBudakovsky <dimon.vb@mail.ru>
2020-04-01 01:00:32 +0300

    Update main.cpp
    [upd] Добавил реализацию функций Selection и Crossingover. В полной их рабоспособности не уверен, но вроде должны работать. Как будут готовы функции нужные для работы моих, потестирую.

--- main.cpp
+++ main.cpp
@@ -49,13 +49,73 @@ int FitnessFunction(vector<bool> X) {
 }
 
 
-vector<vector<bool>> Selection(vector<vector<bool>> Generation) {
-    // TODO DIMA
+vector<vector<bool>> Selection(vector<vector<bool>>& Generation)
+{
+	// TODO DIMA
+	// вычисляем сумму фитнесс-функций всех особей
+	long long sum_fitness_func(0);
+	for (int i(0); i < Generation.size(); ++i)
+		sum_fitness_func += FitnessFunction(Generation[i]);
+
+	// вычисляем количество особей i-хромосомы в промежуточной популяции
+	vector<int> probabilities_individuals;
+	for (int i(0); i < Generation.size(); ++i)
+	{
+		double hit_probability = FitnessFunction(Generation[i]) * Generation.size() / sum_fitness_func;
+		int int_hit_probabilit = round(hit_probability);
+		probabilities_individuals.push_back(int_hit_probabilit);
+	}
+
+	// формируем промежуточную популяцию
+	vector<vector<bool>> intermediate_population;
+	for (int i(0); i < probabilities_individuals.size(); ++i)
+		for (int j(0); j < probabilities_individuals[i]; ++j)
+			intermediate_population.push_back(Generation[i]);
+
+	return intermediate_population;
 }
 
 
-vector<bool> Crossingover(vector<vector<bool>> Generation) {
-    // TODO DIMA
+vector<bool> Crossingover(vector<vector<bool>>& Generation)
+{
+	// TODO DIMA
+	RAND_MAX = Generation.size() - 1;
+	srand(time(0));
+
+	vector<vector<bool>> new_population;
+
+	while (new_population.size() != Generation.size())
+	{
+		int first_parent = rand();
+		int second_parent = rand();
+
+		while ((Generation[first_parent] == Generation[second_parent]) &&
+			(Generation[first_parent].empty()) &&
+			(Generation[second_parent].empty()))
+		{
+			first_parent = rand();
+			second_parent = rand();
+		}
+
+		int point_crossingover = rand() % (Generation[first_parent].size() - 2);
+
+		auto child_1 = Generation[first_parent];
+		auto child_2 = Generation[second_parent];
+
+		for (int j(point_crossingover); j < Generation[first_parent].size(); ++j)
+		{
+			child_1[j] = Generation[second_parent][j];
+			child_2[j] = Generation[first_parent][j];
+		}
+
+		Generation[first_parent].clear();
+		Generation[second_parent].clear();
+
+		new_population.push_back(child_1);
+		new_population.push_back(child_2);
+	}
+
+	return new_population;
 }
 
 
